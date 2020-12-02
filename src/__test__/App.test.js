2eaad6b11989ad03119495541889d1a4763eb554
Czarl Jalos <czarlfrancis@gmail.com>
2020-12-02 10:07:06 -0800

    lab redux

---
+++ src/__test__/App.test.js
@@ -0,0 +1,16 @@
+  
+import mainReducer, { chooseCategory } from '../store/products-categories.js';
+
+it('should have initial state', () => {
+
+  // undefined is being passed in to trigger the default value for state
+  const state = mainReducer(undefined, {});
+
+  expect(state.activeCategory).toEqual('food');
+
+});
+
+it.skip('should modify active category', () => {
+
+
+});
